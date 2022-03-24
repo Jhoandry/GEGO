494defbc160d109984b8f22cc2fa654520e3878a
Jhoandry Sequera <jsequera@optoro.com>
2022-03-24 20:26:51 -0300

    GG-001: first build app

--- app/src/main/java/com/lugloc/activities/MainActivity.kt
+++ app/src/main/java/com/lugloc/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lugloc
+package com.lugloc.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lugloc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
